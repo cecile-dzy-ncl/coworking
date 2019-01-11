c93f6958c228e786a742a6d0a52247c05a4d00ca
Cecile Dezy <cecile.dezy@gmail.com>
2019-01-11 16:08:07 +0100

    no validation for request status

--- app/models/request.rb
+++ app/models/request.rb
@@ -3,5 +3,5 @@ class Request < ApplicationRecord
   validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-zA-Z0-9.]{2,}\.[a-z]{2,4}\z/ }
   validates :phone, presence: true, format: { with: /\A(0|\+33)[1-9]\d{8}\z/ }
   validates :bio, presence: true, length: { minimum: 140 }
-  validates :status, presence: true, default: "unconfirmed"
+  # validates :status, presence: true, default: "unconfirmed"
 end
