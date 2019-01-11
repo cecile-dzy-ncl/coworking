ff64deb72e888d78422fde98fad1ad2c9f8f034a
Cecile Dezy <cecile.dezy@gmail.com>
2019-01-11 15:58:06 +0100

    change regex single line

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,7 +1,7 @@
 class Request < ApplicationRecord
   validates :name, presence: true
-  validates :email, presence: true, format: { with: /^[\w+\-.]+@[a-zA-Z0-9.]{2,}\.[a-z]{2,4}$/ }
-  validates :phone, presence: true, format: { with: /^(0|\+33)[1-9]\d{8}$/ }
+  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-zA-Z0-9.]{2,}\.[a-z]{2,4}\z/ }
+  validates :phone, presence: true, format: { with: /\A(0|\+33)[1-9]\d{8}\z/ }
   validates :bio, presence: true, length: { minimum: 140 }
   validates :status, presence: true, default: "unconfirmed"
 end
