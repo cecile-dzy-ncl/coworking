7aaa50453c7424dacd1a8fd967099e0100e31815
Cecile Dezy <cecile.dezy@gmail.com>
2019-01-13 15:55:45 +0100

    add new / create methods + params

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,4 +1,5 @@
 class RequestsController < ApplicationController
+
   def index
     @requests = Request.all
   end
@@ -7,6 +8,23 @@ class RequestsController < ApplicationController
     @request = Request.new
   end
 
+  def create
+    @request = Request.new(request_params)
+    if @request.save
+      puts "Pour confirmer votre inscription à la liste d'attente, merci de consulter votre boîte mail."
+      redirect_to "/"
+    else
+      puts "Erreur lors de l'enregistrement, merci de recommencer."
+      render :new
+    end
+  end
+
   def edit
+  end
+
+  private
+
+  def request_params
+    params.require(:request).permit(:name, :email, :phone, :bio, :status)
   end
 end
