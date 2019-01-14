439d44b82607a5d9c2071039f6b21a71df4f5a89
Cecile Dezy <cecile.dezy@gmail.com>
2019-01-15 00:44:53 +0100

    change smtp config in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,14 @@
 Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: 'gmail.com',
+    user_name: ENV['GMAIL_ADDRESS'],
+    password: ENV['GMAIL_APP_PASSWORD'],
+    authentication: :login,
+    enable_starttls_auto: true }
   config.action_mailer.default_url_options = { host: "https://wl-coworking.herokuapp.com" }
   # or your custom domain name eg. "www.yourdomain.com"
   # Verifies that versions and hashed value of the package contents in the project's package.json
