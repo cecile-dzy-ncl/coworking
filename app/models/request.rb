8f167bb1b019c4bd0a00fbe0bf2fbdf7f7ee3410
Cecile Dezy <cecile.dezy@gmail.com>
2019-01-14 11:38:22 +0100

    add class methods to enable admin request

--- app/models/request.rb
+++ app/models/request.rb
@@ -4,4 +4,24 @@ class Request < ApplicationRecord
   validates :phone, presence: true, format: { with: /\A(0|\+33)[1-9]\d{8}\z/ }
   validates :bio, presence: true, length: { minimum: 140 }
   # validates :status, presence: true, default: "unconfirmed"
+
+  def self.unconfirmed
+    Request.where(status: "unconfirmed")
+  end
+
+  def self.confirmed
+    Request.where(status: "confirmed")
+  end
+
+  def self.accepted
+    Request.where(status: "accepted")
+  end
+
+  def self.expired
+    Request.where(status: "expired")
+  end
+
+  def accept!
+    self.update(status: "confirmed")
+  end
 end
