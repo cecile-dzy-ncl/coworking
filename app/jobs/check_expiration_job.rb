3083e85d84780ca978301e597659f3f76b964598
Cecile Dezy <cecile.dezy@gmail.com>
2019-01-17 11:49:53 +0100

    remove fake job

--- app/jobs/check_expiration_job.rb
+++ app/jobs/check_expiration_job.rb
@@ -2,12 +2,16 @@ class CheckExpirationJob < ApplicationJob
   queue_as :default
 
   def perform
+    puts "I'm cheking for confirmed request older than 10 minutes and sending mail"
     requests = Request.confirmed
     requests.each do |request|
       # calcul en minutes pour envoi mail relance apres 10 minutes
       if ((Time.now - request.updated_at) / 3600) > 10
         UserMailer.confirmation(request).deliver_now
         request.update(status: "expired")
+        puts "ok i send an email"
+      else
+        puts "no request older than 10 minutes"
       end
     end
   end
--- app/jobs/fake_job.rb
+++ app/jobs/fake_job.rb
@@ -1,9 +1,9 @@
-class FakeJob < ApplicationJob
-  queue_as :default
+# class FakeJob < ApplicationJob
+#   queue_as :default
 
-  def perform
-    puts "I'm starting the fake job"
-    sleep 3
-    puts "OK I'm done now"
-  end
-end
+#   def perform
+#     puts "I'm starting the fake job"
+#     sleep 3
+#     puts "OK I'm done now"
+#   end
+# end
