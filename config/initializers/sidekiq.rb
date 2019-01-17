43fac070144c47835f54b67e2415d761b93d1956
Cecile Dezy <cecile.dezy@gmail.com>
2019-01-17 13:01:19 +0100

    trying another config for rediscloud and sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,11 +1,28 @@
-Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379/0'  }
-  schedule_file = "config/schedule.yml"
-  if File.exists?(schedule_file)
-    Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+if Rails.env.production?
+  Sidekiq.configure_server do |config|
+    config.redis = { url: ENV["REDISCLOUD_URL"]}
+    schedule_file = "config/schedule.yml"
+    if File.exists?(schedule_file)
+      Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+    end
   end
 end
 
+
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379/0'  }
+  config.redis = { url: ENV["REDISTOGO_URL"]}
 end
+
+# ---
+# Sidekiq.configure_server do |config|
+#   config.redis = { url: 'redis://localhost:6379/0'  }
+#   schedule_file = "config/schedule.yml"
+#   if File.exists?(schedule_file)
+#     Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+#   end
+# end
+
+# Sidekiq.configure_client do |config|
+#   config.redis = { url: 'redis://localhost:6379/0'  }
+# end
+# ---
